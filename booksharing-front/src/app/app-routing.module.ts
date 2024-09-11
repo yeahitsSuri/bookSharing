187dde7bf14989aca983dc6032bf7d2d90de618d
Tianshu Yang <yts9614@gmail.com>
2024-09-10 23:53:51 -0700

    implement login page

---
+++ booksharing-front/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Define your routes here
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
