1a18ee19d91dc89429aca51ada624592482f9d0e
Tianshu Yang <yts9614@gmail.com>
2024-09-11 14:01:58 -0700

    handle register

---
+++ booksharing-front/src/app/pages/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { RegistrationRequest } from './../../services/models/registration-request';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { TokenService } from '../../services/token/token.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  registerRequest: RegistrationRequest = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+  };
+  errorMsg: Array<string> = [];
+
+  constructor(private router: Router,
+    private authService: AuthenticationService,
+    private tokenService: TokenService
+  ) { }
+
+  register(): void {
+    this.errorMsg = []; // always clear the error message before setting
+    this.authService.register({
+      body: this.registerRequest
+    }).subscribe({
+      next: () => {
+        this.router.navigate(['/activate-account']);
+      },
+      error: (err) => {
+        this.errorMsg = err.error.validationErrors;
+      }
+    });
+  }
+
+  login(): void {
+    this.router.navigate(['/login']);
+  }
+}
